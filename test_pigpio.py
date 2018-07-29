59678f5be9253e1dfcbf5ebb4b6d9f71ed30643d
Makito Ishikura <tilt.silvie@gmail.com>
2018-07-29 09:17:08 +0000

    add other setting

--- test_pigpio.py
+++ test_pigpio.py
@@ -9,7 +9,10 @@ if not pi.connected:
     exit(0)
 
 
-spi = pi.spi_open(0, 1000000, 0)
+# spi = pi.spi_open(0, 1000000, 0) # main SPI, CE0
+# spi = pi.spi_open(1, 1000000, 0) # main SPI, CE1
+spi = pi.spi_open(0, 1000000, 256) # auxiliary SPI, CE0
+
 
 for i in range(100):
     pi.spi_write(spi, [1, 2, 3, 4, 5])
